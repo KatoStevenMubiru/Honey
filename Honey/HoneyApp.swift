8c1325713ebb66902f62397199f2d65b671b6f43
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-01-29 06:22:14 +0300

    Initial Commit

---
+++ Honey/HoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneyApp.swift
+//  Honey
+//
+//  Created by Kato Steven Mubiru on 29/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct HoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
