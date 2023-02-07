d86e8c4f71b17f020091faf1ecdef7c1272f81a7
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-02-07 20:24:59 +0300

    Update ContentView.swift

--- Honey/View/ContentView.swift
+++ Honey/View/ContentView.swift
@@ -85,6 +85,7 @@ struct ContentView: View {
                         .zIndex(self.isTopCard(cardView: cardView) ? 1 : 0)
                         .offset(x: self.dragState.translation.width, y: self.dragState.translation.height)
                         .scaleEffect(self.dragState.isDragging ? 0.85 : 1.0)
+                        .rotationEffect(Angle(degrees: Double(self.dragState.translation.width/12)))
                         .animation(.interpolatingSpring(stiffness: 120, damping: 120))
                         .gesture(LongPressGesture(minimumDuration: 0.01) .sequenced(before: DragGesture())
                             .updating(self.$dragState, body: { (value, state , transaction ) in
